efa5c9c83e2aa08e79a037cc6717f21cd218012b
Dave Burdick <daveburdick@gmail.com>
2015-12-03 22:35:44 -0700

    JSHint tidying

--- js/sort.js
+++ js/sort.js
@@ -4,51 +4,51 @@ var swSort = angular.module('swSort', ['ng-sortable']);
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
 
 var Model = {
-	saveChoices: function() {
+    saveChoices: function() {
         var saveIDs = [];
         for (var i = 0, len = self.mine.length; i < len; i++) {
-            saveIDs.push(self.mine[i].id)
-        };
+            saveIDs.push(self.mine[i].id);
+        }
         var saveStr = JSON.stringify(saveIDs);
         var urlStr = Base64.encode(saveStr);
-        newUrl = "http://nicetryinternet.com/star-wars-ranker/index.htm?" + urlStr
+        newUrl = "http://nicetryinternet.com/star-wars-ranker/index.htm?" + urlStr;
         $("#shareUrl").text(newUrl);
         $("meta[property='og\\:url']").attr("content", newUrl);
-	},
-	loadChoices: function() {
+    },
+    loadChoices: function() {
         var url = window.location.href;
         if (url.indexOf('?') === -1) {
             return;
         } else {
-    		var loadUrl = url.slice(window.location.href.indexOf('?') + 1).split();
+            var loadUrl = url.slice(window.location.href.indexOf('?') + 1).split();
             var decodeUrl = loadUrl.toString();
             var loadStr = Base64.decode(decodeUrl);
             var loadIDs = JSON.parse(loadStr);
             var loadArray = [];
             for (var i = 0, len = loadIDs.length; i < len; i++) {
-                for (var f = 0, len = self.stories.length; f < len; f++) {
+                for (var f = 0, fen = self.stories.length; f < fen; f++) {
                     if (loadIDs[i] === self.stories[f].id) {
-                        loadArray.push(self.stories[f])
+                        loadArray.push(self.stories[f]);
                     }
                 }
             }
-        };
+        }
         self.$apply(function(){
             self.mine = loadArray;
         });
-	},
-	clearChoices: function() {
-		self.$apply(function(){
-			self.mine = [];
-		});
-	},
+    },
+    clearChoices: function() {
+        self.$apply(function(){
+            self.mine = [];
+        });
+    },
     addSingle: function(id) {
         var toAdd = self.stories[id];
         self.$apply(function(){
             self.mine[self.mine.length] = toAdd;
         });
     }
-}
+};
 
 angular.module('swSort', ['ng-sortable'])
 .controller('swSortCtrl', ['$scope', function ($scope) {
@@ -59,16 +59,16 @@ angular.module('swSort', ['ng-sortable'])
         var toAdd;
         for (var i = 0, len = self.stories.length; i < len; i++) {
             if (self.stories[i].id === id) {
-                toAdd = self.stories[i]
+                toAdd = self.stories[i];
             }
-        };
+        }
         self.mine[self.mine.length] = toAdd;
     };
     $scope.removeSingle = function(item) {
         var toRemove = self.mine.indexOf(item);
         self.mine.splice(toRemove, 1);
     };
-	$scope.stories = [
+    $scope.stories = [
   {
     "id": 1,
     "title": "Episode I: The Phantom Menace",
@@ -466,33 +466,33 @@ angular.module('swSort', ['ng-sortable'])
     "img": 0
   }
 ];
-	$scope.mine = [];
+    $scope.mine = [];
 
-	self = $scope;
+    self = $scope;
 }]);
 
 var View = {
-	init: function() {
-		$("#options .book, #options .video, #options .comment").css('display', 'none');
-	}
-}
+    init: function() {
+        $("#options .book, #options .video, #options .comment").css('display', 'none');
+    }
+};
 
 $(document).ready(function(){
-	$("#movieButton").click(function(){
-		$("#options .film").toggle()
-	});
-	$("#booksCanonButton").click(function(){
-		$("#options .book.canon").toggle()
-	});
-	$("#booksLegendsButton").click(function(){
-		$("#options .book.legends").toggle()
-	});
-	$("#comicsCanonButton").click(function(){
-		$("#options .comment.canon").toggle()
-	});
-	$("#tvButton").click(function(){
-		$("#options .video").toggle();
-	});
-	View.init();
+    $("#movieButton").click(function(){
+        $("#options .film").toggle();
+    });
+    $("#booksCanonButton").click(function(){
+        $("#options .book.canon").toggle();
+    });
+    $("#booksLegendsButton").click(function(){
+        $("#options .book.legends").toggle();
+    });
+    $("#comicsCanonButton").click(function(){
+        $("#options .comment.canon").toggle();
+    });
+    $("#tvButton").click(function(){
+        $("#options .video").toggle();
+    });
+    View.init();
     Model.loadChoices();
 });
